45a147f16ea68d51ec1b51489752f1e1d3141c61
dneog <debashisneog23@gmail.com>
2023-04-07 09:29:04 +0530

    Expense List

---
+++ src/Components/Expenselist.js
@@ -0,0 +1,13 @@
+function Expenselist(){
+    return (
+        <div>
+            <h2>Expense List</h2>
+            <ul>
+                <li>Food RS 10</li>
+                <li>Petrol RS 100</li>
+                <li>Movies RS 200</li>
+            </ul>
+        </div>
+    )
+}
+export default Expenselist
